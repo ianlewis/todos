0807a15300ee1b32e1d59315f2a51cd224bf93de
Ian Lewis <ianlewis@google.com>
2023-06-03 05:22:05 +0000

    Add tests
    Signed-off-by: Ian Lewis <ianlewis@google.com>

---
+++ internal/cmd/todos/utils_other_test.go
@@ -0,0 +1,62 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//go:build !windows
+
+package main
+
+import (
+	"testing"
+
+	"github.com/ianlewis/todos/internal/testutils"
+)
+
+func Test_isHidden(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		name   string
+		hidden bool
+	}{
+		{
+			name:   "foo.bar",
+			hidden: false,
+		},
+		{
+			name:   "some/path/foo.bar",
+			hidden: false,
+		},
+		{
+			name:   ".foo",
+			hidden: true,
+		},
+		{
+			name:   "some/path/.foo",
+			hidden: true,
+		},
+	}
+
+	for i := range testCases {
+		tc := testCases[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			// NOTE: For *nix we don't actually have to create the file.
+			if got, want := testutils.Must(isHidden(tc.name)), tc.hidden; got != want {
+				t.Errorf("unexpected result for %q, got: %v, want: %v", tc.name, got, want)
+			}
+		})
+	}
+}
---
+++ internal/cmd/todos/utils_windows_test.go
@@ -0,0 +1,102 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//go:build windows
+
+package main
+
+func setHidden(path string) error {
+	filenameW, err := syscall.UTF16PtrFromString(path)
+	if err != nil {
+		return err
+	}
+
+	err = syscall.SetFileAttributes(filenameW, syscall.FILE_ATTRIBUTE_HIDDEN)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func Test_isHidden(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		name       string
+		hiddenAttr bool
+		expected   bool
+	}{
+		{
+			name:       "foo.bar",
+			hiddenAttr: false,
+			expected:   false,
+		},
+		{
+			name:       "some/path/foo.bar",
+			hiddenAttr: false,
+			expected:   false,
+		},
+		{
+			name:       "foo.bar",
+			hiddenAttr: true,
+			expected:   true,
+		},
+		{
+			name:       "some/path/foo.bar",
+			hiddenAttr: true,
+			expected:   true,
+		},
+		{
+			name:       ".foo.bar",
+			hiddenAttr: false,
+			expected:   true,
+		},
+		{
+			name:       "some/path/.foo.bar",
+			hiddenAttr: false,
+			expected:   true,
+		},
+		{
+			name:       ".foo.bar",
+			hiddenAttr: true,
+			expected:   true,
+		},
+		{
+			name:       "some/path/.foo.bar",
+			hiddenAttr: true,
+			expected:   true,
+		},
+	}
+
+	for i := range testCases {
+		tc := testCases[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			dir := testutils.Must(os.MkdirTemp("", tc.name))
+			path := filepath.Join(dir, tc.name)
+			testutils.Check(os.WriteFile(path, "", 0600))
+			if tc.hidden {
+				testutils.Check(setHidden(path))
+			}
+			defer os.RemoveAll(dir)
+
+			if got, want := testutils.Must(isHidden(tc.name)), tc.hidden; got != want {
+				t.Errorf("unexpected result for %q, got: %v, want: %v", tc.name, got, want)
+			}
+		})
+	}
+}
