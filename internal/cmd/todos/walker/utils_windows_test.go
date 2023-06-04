41461ce2583bf3bdf106203e95bffe50700d70a2
Ian Lewis <ianlewis@google.com>
2023-06-04 06:27:48 +0000

    change file perms
    Signed-off-by: Ian Lewis <ianlewis@google.com>

--- internal/cmd/todos/walker/utils_windows_test.go
+++ internal/cmd/todos/walker/utils_windows_test.go
@@ -97,7 +97,7 @@ func Test_isHidden(t *testing.T) {
 
 			dir := testutils.Must(os.MkdirTemp("", "utils_windows_test"))
 			path := filepath.Join(dir, tc.name)
-			testutils.Check(os.WriteFile(path, nil, 0600))
+			testutils.Check(os.WriteFile(path, nil, 0o666))
 			if tc.hiddenAttr {
 				testutils.Check(setHidden(path))
 			}
