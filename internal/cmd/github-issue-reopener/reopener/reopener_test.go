5d5fc93a137f045f1fb4349a3a30f8b93bc14882
Ian Lewis <ianmlewis@gmail.com>
2023-06-13 11:21:39 +0000

    Add handleTODO tests
    Signed-off-by: Ian Lewis <ianmlewis@gmail.com>

--- internal/cmd/github-issue-reopener/reopener/reopener_test.go
+++ internal/cmd/github-issue-reopener/reopener/reopener_test.go
@@ -15,9 +15,15 @@
 package reopener
 
 import (
+	"context"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
+
+	"github.com/ianlewis/todos/internal/cmd/github-issue-reopener/options"
+	"github.com/ianlewis/todos/internal/todos"
+	"github.com/ianlewis/todos/internal/walker"
 )
 
 func matchEqual(got, want []string) bool {
@@ -89,13 +95,144 @@ func Test_labelMatch(t *testing.T) {
 				"123",
 			},
 		},
+		"number only match": {
+			label: "123",
+			match: nil,
+		},
+		"regression test": {
+			label: "github.com/ianlewis/todos/123",
+			match: nil,
+		},
 	}
 
 	for name, tc := range testCases {
 		tc := tc
 		t.Run(name, func(t *testing.T) {
 			if got, want := labelMatch.FindStringSubmatch(tc.label), tc.match; !matchEqual(got, want) {
-				t.Errorf("unexpected match (-want +got):\n%s", cmp.Diff(want, got))
+				t.Fatalf("unexpected match (-want +got):\n%s", cmp.Diff(want, got))
+			}
+		})
+	}
+}
+
+func Test_handleTODO(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		owner    string
+		repo     string
+		ref      []*walker.TODORef
+		expected map[int]*IssueRef
+		err      error
+	}{
+		"test.go": {
+			ref: []*walker.TODORef{
+				{
+					FileName: "test.go",
+					TODO: &todos.TODO{
+						Type:        "TODO",
+						Text:        "// TODO(#123): Foo",
+						Label:       "#123",
+						Message:     "Foo",
+						Line:        5,
+						CommentLine: 5,
+					},
+				},
+			},
+			expected: map[int]*IssueRef{
+				123: {
+					ID: 123,
+					TODOs: []*walker.TODORef{
+						{
+							FileName: "test.go",
+							TODO: &todos.TODO{
+								Type:        "TODO",
+								Text:        "// TODO(#123): Foo",
+								Label:       "#123",
+								Message:     "Foo",
+								Line:        5,
+								CommentLine: 5,
+							},
+						},
+					},
+				},
+			},
+		},
+		"multi-references": {
+			owner: "ianlewis",
+			repo:  "todos",
+			ref: []*walker.TODORef{
+				{
+					FileName: "test.go",
+					TODO: &todos.TODO{
+						Type:        "TODO",
+						Text:        "// TODO(#123): Foo",
+						Label:       "#123",
+						Message:     "Foo",
+						Line:        5,
+						CommentLine: 5,
+					},
+				},
+				{
+					FileName: "other.go",
+					TODO: &todos.TODO{
+						Type:        "TODO",
+						Text:        "// TODO(github.com/ianlewis/todos/123): Bar",
+						Label:       "github.com/ianlewis/todos/issues/123",
+						Message:     "Bar",
+						Line:        5,
+						CommentLine: 5,
+					},
+				},
+			},
+			expected: map[int]*IssueRef{
+				123: {
+					ID: 123,
+					TODOs: []*walker.TODORef{
+						{
+							FileName: "test.go",
+							TODO: &todos.TODO{
+								Type:        "TODO",
+								Text:        "// TODO(#123): Foo",
+								Label:       "#123",
+								Message:     "Foo",
+								Line:        5,
+								CommentLine: 5,
+							},
+						},
+						{
+							FileName: "other.go",
+							TODO: &todos.TODO{
+								Type:        "TODO",
+								Text:        "// TODO(github.com/ianlewis/todos/123): Bar",
+								Label:       "github.com/ianlewis/todos/issues/123",
+								Message:     "Bar",
+								Line:        5,
+								CommentLine: 5,
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	for name, tc := range testCases {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			r := New(context.Background(), &options.Options{
+				RepoOwner: tc.owner,
+				RepoName:  tc.repo,
+			})
+			for i := range tc.ref {
+				err := r.handleTODO(tc.ref[i])
+				if diff := cmp.Diff(tc.err, err, cmpopts.EquateErrors()); diff != "" {
+					t.Fatalf("unexpected error (-want +got):\n%s", diff)
+				}
+			}
+
+			if diff := cmp.Diff(tc.expected, r.refs.cache, cmpopts.EquateErrors()); diff != "" {
+				t.Fatalf("unexpected result (-want +got):\n%s", diff)
 			}
 		})
 	}
