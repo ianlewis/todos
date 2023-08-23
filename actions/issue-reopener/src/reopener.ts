03fa31387958460981e9b5cd475c324be149f65f
Ian Lewis <ianlewis@google.com>
2023-08-23 15:43:23 +0900

    chore(release): Update for v0.4.0-rc.1 (#256)
    Fixes #240
    
    ---------
    
    Signed-off-by: Ian Lewis <ianlewis@google.com>

--- actions/issue-reopener/src/reopener.ts
+++ actions/issue-reopener/src/reopener.ts
@@ -20,7 +20,7 @@ import * as github from "@actions/github";
 
 import * as verifier from "./verifier";
 
-const TODOS_VERSION = "v0.4.0-rc0";
+const TODOS_VERSION = "v0.4.0-rc.1";
 const SLSA_VERIFIER_VERSION = "v2.3.0";
 const SLSA_VERIFIER_SHA256SUM =
   "ea687149d658efecda64d69da999efb84bb695a3212f29548d4897994027172d";
